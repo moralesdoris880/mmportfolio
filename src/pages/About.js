8b713cd2fb91257815970cc10abf97eac20ff7ae
Doris Morales <moralesdoris880@gmail.com>
2023-01-01 20:54:50 -0500

    contact logos added

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,15 @@ function About() {
       <div className=" h-full flex bg-gray-900">
         <img src="/aboutme.jpg" className="border-2 ml-36 mt-20 w-11/12 h-3/4"/>
         <div className="flex-col">
-        <p>About Me</p>
-        <p>Nice to meet you! My name is Meraz. I am an aspiring data analyst based in NYC. I have experience making dynamic analytic dashboards on Excel, using MySQL to query data, and scraping data from different websites through R and Python. Outside of work, I enjoy exercising and listening to true crime podcasts.</p>
-        <p>Contact Me</p>
+        <p className="tracking-wide text-gray-100 text-4xl font-medium font-mono">About Me</p>
+        <p className="text-gray-100">Nice to meet you! My name is Meraz. I am an aspiring data analyst based in NYC. I have experience making dynamic analytic dashboards on Excel, using MySQL to query data, and scraping data from different websites through R and Python. Outside of work, I enjoy exercising and listening to true crime podcasts.</p>
+        <p className="text-gray-100 text-3xl font-medium font-mono tracking-wide">Contact Me</p>
+        
+        <div className="flex space-x-4 m-2">
+          <img className="w-1/12 h-2/4" src="/email.png"></img> 
+          <img className="w-1/12 h-2/4" src="/linkedin.png"></img>
+          <img className="w-1/12 h-2/4" src="/youtube.png"></img>
+        </div>
         </div>
       </div>
     );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,24 @@
 function Projects() {
     return (
       <div>
-        <p>Projects Page</p>
+        <p>Data Analysis</p>
+
+        <div>
+            <h1>Cyclistic Case Study</h1>
+            <p> The goal of this case is to analyze how casual riders and annual members use Cyclistic bikes differently and apply this analysis to convert more casual riders into annual members.</p>
+
+            <h1>Ask Phase</h1>
+            <p>How do annual members and casual riders use Cyclistic bikes differently?</p>
+            <p>Why would casual riders buy Cyclistic annual memberships?</p>
+            <p>How can Cyclistic use digital media to influence casual riders to become members?</p>
+            <p>How long do each rider use bikes, organized by rideable type (and casual / member), over the past year on a weekly and monthly basis?</p>
+            <p>How has the percentage of casual-to-members changed over the past year?</p>
+
+            <h1>Key Stakeholders</h1>
+            <p>Lily Moreno: Cyclistic's director of marketing and manager</p>
+            <p>Cyclistic Marketing Analytics Team: A team of data analysts who are responsible for collecting, analyzing, and reporting data that helps guide Cyclistic marketing strategy</p>
+            <p>Cyclistic Executive Team: Will decide whether to approve the recommended marketing program</p>
+        </div>
       </div>
     );
   }
