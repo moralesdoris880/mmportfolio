7539dd68e536feb2932d3e0307193acd09b0db1a
Doris Morales <moralesdoris880@gmail.com>
2023-01-15 19:40:49 -0500

    Added Carousel Library w/ content on visuals

--- src/components/GraphCarousel.js
+++ src/components/GraphCarousel.js
@@ -1,14 +1,43 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 function GraphCarousel(){
     return(
-        <div>
+        <div className="w-1/2">
+            <Carousel>
+            <div>
+                <img src="/graph0.png" />
+                {/* <p className="legend">Enter Description</p> */}
+            </div> 
             <div>
                 <img src="/graph1.png" />
-                <p className="legend">Legend 2</p>
+                {/* <p className="legend">Enter Description</p> */}
             </div>
             <div>
                 <img src="/graph2.png" />
-                <p className="legend">Legend 1</p>
+                {/* <p className="legend">Enter Description</p> */}
+            </div>
+            <div>
+                <img src="/graph3.png" />
+                {/* <p className="legend">Enter Description</p> */}
+            </div> 
+            <div>
+                <img src="/graph4.png" />
+                {/* <p className="legend">Enter Description</p> */}
             </div>  
+            <div>
+                <img src="/graph5.png" />
+                {/* <p className="legend">Enter Description</p> */}
+            </div> 
+            <div>
+                <img src="/graph6.png" />
+                {/* <p className="legend">Enter Description</p> */}
+            </div> 
+            <div>
+                <img src="/graph7.png" />
+                {/* <p className="legend">Enter Description</p> */}
+            </div> 
+            </Carousel> 
         </div>
     );
 }
