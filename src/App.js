ca0ecad93191f2b94135d609fc7897cf20c629de
Doris Morales <moralesdoris880@gmail.com>
2023-01-17 19:39:38 -0500

    add redirect

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ function App() {
         <h1 className="mb-2 font-mono text-4xl text-gray-100 md:text-6xl">
           Hi, I&apos;m <br className="block md:hidden" />
           <span className="relative">
-          <span className="h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
-          Meraz Mamun <span className="text-3xl md:text-5xl">👋</span>
-          </span>
-          <span
-          className="{`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-gray-900 w-full animate-type will-change`}"
-          ></span>
+            <span className="h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
+            Meraz Mamun <span className="text-3xl md:text-5xl">👋</span>
+            </span>
+            <span className="{`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-gray-900 w-full animate-type will-change`}"></span>
           </span>
         </h1> 
         {/* Credit to https://vanntile.com/blog/composited-typing-animation for their tutorial */}
