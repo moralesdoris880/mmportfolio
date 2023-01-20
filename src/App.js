dc59fc2e75baa24f44ee25c3b5abc0efdc371ee6
Doris Morales <moralesdoris880@gmail.com>
2023-01-20 17:41:55 -0500

    Emoji color adjusted and home page image changed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {Link} from "react-router-dom";
 function App() {
   return (
     <div className="flex flex-row justify-around items-center bg-gray-900 h-[94%]">
-      <div className="m-8">
+      <div className="ml-40">
         <h1 className="mb-2 font-mono text-4xl text-gray-100 md:text-6xl">
           Hi, I&apos;m <br className="block md:hidden" />
           <span className="relative">
             <span className="h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
-            Meraz Mamun <span className="text-3xl md:text-5xl">👋</span>
+            Meraz Mamun <span className="text-3xl md:text-5xl">👋🏾</span>
             </span>
             <span className="{`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-gray-900 w-full animate-type will-change`}"></span>
           </span>
@@ -19,8 +19,8 @@ function App() {
           Projects
         </button></Link>
       </div> 
-      <div className="z-40">
-        <img className="mr-10 rounded-full border-2" alt="Potrait of Meraz Mamun" src="https://media.licdn.com/dms/image/C4D03AQHojaipPZptxg/profile-displayphoto-shrink_400_400/0/1569163475506?e=1678924800&v=beta&t=dxfqMv1gLcHf3NURRAmXiDewqUrlFSt0IUAao6I5anI"/>
+      <div className="z-40 w-1/2">
+        <img className="rounded-full border-2 w-3/4" alt="Potrait of Meraz Mamun" src="/meraz.jpg"/>
       </div>
     </div>
   );
