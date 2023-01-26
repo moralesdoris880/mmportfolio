f457865ccb54cdffdf6238ee2f854e926ae4a05f
Doris Morales <moralesdoris880@gmail.com>
2023-01-25 20:54:17 -0500

    Clean out comments

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,6 @@
-// import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-
 function Resume() {
     return (
       <div className='flex items-center bg-gray-900'>
-        {/* <Document file="Meraz_Mamun_Resume.pdf" className="scale-75 ml-60">
-          <Page pageNumber={1} />
-        </Document> */}
         <img src='resumeimg.png' className='h-[690px] ml-80 mr-10 mb-20 mt-4' alt="meraz_resume" />
         <button className='ml-50 h-14 w-40 bg-gradient-to-r from-cyan-500 to-blue-500 text-gray-100 text-2xl tracking-wide font-mono'>Download</button>
       </div>
